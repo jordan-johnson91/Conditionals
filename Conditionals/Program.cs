a010a7dba8b80d2e34e356b13a70a57d1b1c9a34
Jordan Johnson <jordan@wecancodeit.org>
2017-08-17 20:51:47 -0400

    Added conditionals

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,19 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your age");
+
+            int age = int.Parse(Console.ReadLine());
+
+            if(age >= 17)
+            {
+                Console.WriteLine("You can see this movie");
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry you are too young you need a parent present");
+            }
         }
     }
 }
